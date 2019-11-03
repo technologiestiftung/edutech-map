444b1a6e7440e8b94796649640343eec404c28f9
fdnklg <fabian.dinklage@me.com>
2019-11-03 13:55:47 +0100

    fetch to api test

--- src/state/Actions.js
+++ src/state/Actions.js
@@ -1,10 +1,12 @@
 import fetch from 'unfetch';
+import config from '../../config';
 
 const loadData = (Store) => async () => {
   Store.setState({ isLoading: true });
 
   let data = null;
   try {
+    console.log(config)
     const res = await fetch('public/data/sample.json');
     data = await res.json();
   } catch (err) {
