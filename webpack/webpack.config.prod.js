c9fee3b522d50211a551b88413f802b2bc211176
ff6347 <hash@ff6347.email>
2023-02-14 20:35:30 +0100

    test deploy to gh pages

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,52 +1,50 @@
-const Path = require('path');
-const Webpack = require('webpack');
-const merge = require('webpack-merge');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const Autoprefixer = require('autoprefixer');
+const Path = require("path");
+const Webpack = require("webpack");
+const merge = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const Autoprefixer = require("autoprefixer");
 
-const common = require('./webpack.common.js');
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: 'production',
-  devtool: 'source-map',
-  stats: 'errors-only',
-  bail: true,
-  output: {
-    filename: 'js/[name].[chunkhash:8].js',
-    chunkFilename: 'js/[name].[chunkhash:8].chunk.js'
-  },
-  plugins: [
-    new Webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    new MiniCssExtractPlugin({ filename: 'bundle.css' }),
-    new Webpack.optimize.ModuleConcatenationPlugin()
-  ],
-  resolve: {
-    alias: {
-      '~': Path.resolve(__dirname, '../src')
-    }
-  },
-  module: {
-    noParse: /(mapbox-gl)\.js$/,
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true,
-              ident: 'postcss',
-              plugins: () => [
-                Autoprefixer
-              ]
-            }
-          }
-        ]
-      }
-    ]
-  }
+	mode: "production",
+	devtool: "source-map",
+	stats: "errors-only",
+	bail: true,
+	output: {
+		filename: "js/[name].[chunkhash:8].js",
+		chunkFilename: "js/[name].[chunkhash:8].chunk.js",
+	},
+	plugins: [
+		new Webpack.DefinePlugin({
+			"process.env.NODE_ENV": JSON.stringify("production"),
+		}),
+		new MiniCssExtractPlugin({ filename: "bundle.css" }),
+		new Webpack.optimize.ModuleConcatenationPlugin(),
+	],
+	resolve: {
+		alias: {
+			"~": Path.resolve(__dirname, "src"),
+		},
+	},
+	module: {
+		noParse: /(mapbox-gl)\.js$/,
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader",
+					{
+						loader: "postcss-loader",
+						options: {
+							sourceMap: true,
+							ident: "postcss",
+							plugins: () => [Autoprefixer],
+						},
+					},
+				],
+			},
+		],
+	},
 });
