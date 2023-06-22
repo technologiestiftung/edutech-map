75c6ffe56b3c50d99c0100747129388be37b0b5a
herrherrmann <sebastian@herrherrmann.net>
2023-06-22 15:08:28 +0000

    Deploying to gh-pages from @ technologiestiftung/edutech-map@6915915cb55df2b083d97a1c46635d80533509bd 🚀

---
+++ public/data-structure.js
@@ -0,0 +1,54 @@
+const structure = {
+  Gruendung: "2019-0101",
+
+  Rechtsform: ["Gewerblich", "Gemeinnützig"],
+
+  Adresse: "Rostocker Straße 68, 2099 Hamburg",
+
+  Ansprechpartner: "mail@name.de", // Nur eine Mail Adresse
+
+  Webseite: "adresse.com", // Nur eine Info ggf. aufteilen?
+
+  Mitarbeiter: "1-15",
+
+  Art: ["Bildungsadministration", "Bildungsinstitution", "Coworking Space", "Hochschule", "Inkubator", "Netzwerk", "Unternehmen", "Andere", "Welche genau?__1"],
+
+  Anwendung: ["Administration", "Bibliotheken", "Bildungsinstitutionen", "Museen", "Pädagog*innen", "Unternehmen__1", "Vereine / Initiativen", "Andere__1", "Welche genau?__2"],
+
+  Zielmarkt: ["Familie", "Kita/Vorschule", "Lifelong Learning", "Schüler", "Studierende", "Andere__2", "Welche genau?__3", "Welcher Bereich genau?"],
+
+  Beschreibung: "Fobizz ist Deutschlands größte, unabhängige Weiterbildungsplattform..."
+
+  AngebotKategorie: "Services / Plattformen / Dienstleistungen", // Uneinheitliche Daten
+
+  "Angebote": ["Welche genau?__4", "Welche genau?__5", "Welche genau?__6", "Welche genau?__7", "Welche genau?__8", "Welche genau?__9", "Welche genau?__10", "Welche genau?__11"], // Uneinheitliche Daten
+}
+
+object = {
+  Rechtsform
+}
+
+Unternehmensdaten:
+- Titel (Nicht im Datensatz vorhanden)
+- Gründung
+- Art d. Institution (z.B. Inkubator)
+- Adresse
+- Webseite
+- Beschreibung
+
+Geografische Verordnung:
+- Geokoordinaten // Nur möglich, wenn Adresse korrekt angegeben wird
+
+Zielgruppen:
+z.B. Familie, Kita/Vorschule
+
+Produktgruppen:
+- Uneinheitlich z.B. Fort- & Weiterbildung
+
+
+Grafische Darstellung von
+- Unternehmensdaten
+- geografische Verortung
+- Zielgruppen (Personengruppen/Schulstufen/Alter)
+- Zielmarkt (Schule, Nachmittagsbereich)
+- Produktgruppen
